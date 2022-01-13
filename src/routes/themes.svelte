e9051a37a8dfe795e3f30045e343b54171cd8bc9
mvllow <mvllow@icloud.com>
2022-01-13 11:13:44 -0600

    suggest action when no matches are found

--- src/routes/themes.svelte
+++ src/routes/themes.svelte
@@ -108,33 +108,49 @@
 	<ul
 		class="mt-12 mx-auto gap-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4"
 	>
-		{#each filteredThemes as theme}
-			<Transition
-				active="delay-100 duration-500 transition-[opacity,transform] ease-out"
-				from="opacity-0 translate-y-6"
-				to="opacity-1 translate-y-0"
+		{#if filteredThemes.length < 1}
+			<div
+				class="mx-auto py-12 w-full max-w-lg h-full col-span-full bg-gradient-to-br from-surface dark:from-overlay via-base to-surface dark:to-base bg-[length:200%_200%] bg-left-top hover:bg-right-bottom shadow dark:shadow-none duration-200 transition-all ease-in-out rounded-2xl flex flex-col items-center"
 			>
-				<li>
+				<p class="font-display">{$_('page.themes.no-match.title')}</p>
+				<p class="mt-4 text-sm font-medium">
 					<a
-						href={theme.repo}
-						class="py-12 h-full bg-gradient-to-br from-surface dark:from-overlay via-base to-surface dark:to-base bg-[length:200%_200%] bg-left-top hover:bg-right-bottom shadow dark:shadow-none duration-200 transition-[background-position,box-shadow] ease-in-out rounded-2xl flex flex-col items-center focus:outline-none focus:ring focus:ring-highlight-high"
+						href="#!"
+						class="text-iris underline underline-offset-4 decoration-wavy decoration-subtle rounded focus:outline-none focus:ring focus:ring-highlight-high"
+						>{$_('page.themes.no-match.action')}</a
 					>
-						<p
-							class="font-display font-semibold text-lg text-center tracking-wide"
+					{$_('page.themes.no-match.message')}
+				</p>
+			</div>
+		{:else}
+			{#each filteredThemes as theme}
+				<Transition
+					active="delay-100 duration-500 transition-[opacity,transform] ease-out"
+					from="opacity-0 translate-y-6"
+					to="opacity-1 translate-y-0"
+				>
+					<li>
+						<a
+							href={theme.repo}
+							class="py-12 h-full bg-gradient-to-br from-surface dark:from-overlay via-base to-surface dark:to-base bg-[length:200%_200%] bg-left-top hover:bg-right-bottom shadow dark:shadow-none duration-200 transition-[background-position,box-shadow] ease-in-out rounded-2xl flex flex-col items-center focus:outline-none focus:ring focus:ring-highlight-high"
 						>
-							{theme.shortname || theme.name}
-						</p>
+							<p
+								class="font-display font-semibold text-lg text-center tracking-wide"
+							>
+								{theme.shortname || theme.name}
+							</p>
 
-						<div class="mt-4 space-x-3 flex items-center">
-							<MoonIcon class="w-4 h-4 text-subtle" />
+							<div class="mt-4 space-x-3 flex items-center">
+								<MoonIcon class="w-4 h-4 text-subtle" />
 
-							{#if theme.variants}
-								<SunIcon class="w-5 h-5 text-gold" />
-							{/if}
-						</div>
-					</a>
-				</li>
-			</Transition>
-		{/each}
+								{#if theme.variants}
+									<SunIcon class="w-5 h-5 text-gold" />
+								{/if}
+							</div>
+						</a>
+					</li>
+				</Transition>
+			{/each}
+		{/if}
 	</ul>
 </Section>
