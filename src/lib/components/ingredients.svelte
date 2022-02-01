80705fd402656fa00a964456e17de7c9e3fcec67
fvrests <fvrests@icloud.com>
2022-01-31 19:07:00 -0600

    add unstyled color format

--- src/lib/components/ingredients.svelte
+++ src/lib/components/ingredients.svelte
@@ -2,9 +2,6 @@
 	import palette from '@rose-pine/palette'
 	import { browser } from '$app/env'
 
-	const variants = Object.keys(palette.variants)
-	const roles = Object.keys(palette.roles)
-
 	let copiedItem = ''
 	let copiedTimeout = setTimeout
 
@@ -18,6 +15,51 @@
 			}, 600)
 		}
 	}
+
+	const formats = ['default', 'unstyled']
+	type Formats = 'default' | 'unstyled'
+	let colorFormat: Formats = 'default'
+
+	if (browser) {
+		colorFormat = localStorage.getItem('color-format') || 'default'
+		if (!formats.includes(colorFormat)) {
+			colorFormat = 'default'
+		}
+	}
+
+	$: {
+		if (browser) {
+			localStorage.setItem('color-format', colorFormat)
+		}
+	}
+
+	function format(color, mode?: Formats) {
+		if (mode === 'unstyled')
+			return {
+				hex: color.hex.replace('#', ''),
+				rgb: color.rgb.replace('rgb(', '').replace(')', ''),
+				hsl: color.hsl.replace('hsl(', '').replace(')', ''),
+			}
+
+		return color
+	}
+
+	let colors = { default: {}, unstyled: {} }
+
+	const variants = Object.keys(palette.variants)
+	const roles = Object.keys(palette.roles)
+
+	variants.map((variant) => {
+		colors.default[variant] = {}
+		colors.unstyled[variant] = {}
+
+		roles.map((role) => {
+			let currentColor = palette.variants[variant][role]
+
+			colors.default[variant][role] = format(currentColor, 'default')
+			colors.unstyled[variant][role] = format(currentColor, 'unstyled')
+		})
+	})
 </script>
 
 {#each variants as variant}
@@ -30,13 +72,38 @@
 
 	<div class="py-20">
 		<div class="p-6 bg-surface rounded-md">
-			<div class="pb-6">
+			<div class="pb-6 flex items-center justify-between">
 				<h4
 					id="{variant}-ingredients"
 					class="leading-none font-display font-semibold text-lg tracking-wide max-w-full"
 				>
 					{variantName}
 				</h4>
+
+				<div class="relative flex items-center pl-1">
+					<label for="color-format" class="sr-only">
+						<!-- {$_('component.language.label')} -->
+					</label>
+
+					<select
+						id="color-format"
+						bind:value={colorFormat}
+						class="pr-7 pl-2 py-1 w-full leading-normal text-sm text-subtle bg-highlight-low rounded-md appearance-none focus:outline-none focus:ring focus:ring-highlight-high"
+					>
+						<option value={colorFormat}>Format</option>
+						<option value="default" disabled={colorFormat === 'default'}
+							>Default</option
+						>
+						<option value="unstyled" disabled={colorFormat === 'unstyled'}
+							>Unstyled</option
+						>
+					</select>
+
+					<span
+						aria-hidden="true"
+						class="mt-px mr-2 text-subtle border-t-[6px] border-t-muted border-x-4 border-x-transparent inline-block align-middle absolute right-0 z-10"
+					/>
+				</div>
 			</div>
 			<div class="overflow-x-scroll">
 				<table class="w-full min-w-max">
@@ -51,7 +118,7 @@
 
 					<tbody>
 						{#each roles as role}
-							{@const color = palette.variants[variant][role]}
+							{@const color = colors[colorFormat][variant][role]}
 							{@const colorName =
 								role === 'highlightLow'
 									? 'highlight low'
@@ -72,7 +139,7 @@
 									<div class="flex items-center">
 										<div
 											class="mr-3 w-[18px] h-[18px] border rounded-full"
-											style:background={color.hex}
+											style:background={colors.default[variant][role].hex}
 											style:border-color={borderColor}
 										/>
 										<span class="font-medium text-md tracking-wide"
