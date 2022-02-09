68db734071219b567d0b310100cf91f3f41e7ca3
mvllow <mvllow@icloud.com>
2022-02-08 19:13:17 -0600

    add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-netlify'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
