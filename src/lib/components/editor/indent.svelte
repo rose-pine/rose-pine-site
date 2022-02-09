8728e4f3604fdf1bd60bbb5b1080fc678e7ad315
not <mvllow@icloud.com>
2022-02-08 19:08:33 -0600

    Improve enter transitions (#20)

--- src/lib/components/editor/indent.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let level = 1
-
-	let indentBy = `${level * 48}px`
-</script>
-
-<div class="shrink-0" style="width: {indentBy}" />
--- src/lib/components/editor/index.svelte
+++ src/lib/components/editor/index.svelte
@@ -2,7 +2,6 @@
 	import Window from '$lib/components/editor/window.svelte'
 	import Line from '$lib/components/editor/line.svelte'
 	import Blob from '$lib/components/editor/blob.svelte'
-	import Indent from '$lib/components/editor/indent.svelte'
 </script>
 
 <Window>
@@ -30,15 +29,13 @@
 		<Blob size="xs" color="subtle" />
 	</Line>
 
-	<Line>
-		<Indent level={1} />
+	<Line indent={1}>
 		<Blob size="xs" color="subtle" />
 		<Blob size="lg" color="subtle" />
 		<Blob size="md" color="subtle" />
 	</Line>
 
-	<Line>
-		<Indent level={1} />
+	<Line indent={1}>
 		<Blob size="md" color="love" />
 		<Blob size="2xl" color="text" />
 		<Blob size="lg" color="gold" />
@@ -47,8 +44,7 @@
 
 	<Line />
 
-	<Line>
-		<Indent level={1} />
+	<Line indent={1}>
 		<Blob size="sm" color="pine" />
 		<Blob size="xl" color="text" />
 		<Blob size="lg" color="iris" />
@@ -56,49 +52,42 @@
 		<Blob size="xs" color="subtle" />
 	</Line>
 
-	<Line>
-		<Indent level={2} />
+	<Line indent={2}>
 		<Blob size="md" color="foam" />
 		<Blob size="sm" color="gold" />
 	</Line>
 
-	<Line>
-		<Indent level={2} />
+	<Line indent={2}>
 		<Blob size="md" color="foam" />
 		<Blob size="2xl" color="rose" />
 	</Line>
 
-	<Line>
-		<Indent level={2} />
+	<Line indent={2}>
 		<Blob size="sm" color="foam" />
 		<Blob size="md" color="gold" />
 	</Line>
 
-	<Line>
-		<Indent level={1} />
+	<Line indent={1}>
 		<Blob size="xs" color="subtle" />
 	</Line>
 
 	<Line />
 
-	<Line>
-		<Indent level={1} />
+	<Line indent={1}>
 		<Blob size="lg" color="foam" />
 		<Blob size="md" color="iris" />
 		<Blob size="xs" color="pine" />
 		<Blob size="sm" color="gold" />
 	</Line>
 
-	<Line>
-		<Indent level={1} />
+	<Line indent={1}>
 		<Blob size="xl" color="foam" />
 		<Blob size="lg" color="iris" />
 		<Blob size="xs" color="pine" />
 		<Blob size="md" color="gold" />
 	</Line>
 
-	<Line>
-		<Indent level={1} />
+	<Line indent={1}>
 		<Blob size="sm" color="foam" />
 		<Blob size="xl" color="iris" />
 		<Blob size="xs" color="pine" />
