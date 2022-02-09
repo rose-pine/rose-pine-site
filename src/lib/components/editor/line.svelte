4b926eb5ee1bdaf5645f6141bd01c48d8e8f02d5
mvllow <mvllow@icloud.com>
2022-02-08 18:53:30 -0600

    handle editor indents in line component

--- src/lib/components/editor/line.svelte
+++ src/lib/components/editor/line.svelte
@@ -1,5 +1,15 @@
+<script>
+	export let indent = 0
+
+	let indentBy = `${indent * 48}px`
+</script>
+
 <div
 	class="flex h-7 items-center space-x-3 bg-base px-10 hover:bg-highlight-low"
 >
+	{#if indent > 0}
+		<div class="shrink-0" style="width: {indentBy}" />
+	{/if}
+
 	<slot />
 </div>
