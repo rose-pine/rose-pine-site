4f7d3828bfa237c6e75c5f2d480a62f88aec865b
mvllow <mvllow@icloud.com>
2022-07-01 16:03:18 -0400

    fix: nested active nav items

--- src/lib/components/nav-item.svelte
+++ src/lib/components/nav-item.svelte
@@ -3,18 +3,17 @@
 
 	export let href = ''
 	export let label = ''
-	export let showActive = true
 </script>
 
 <a
 	{href}
 	class="w-full rounded-md px-2 py-1 text-sm text-subtle hover:bg-highlight-low focus:outline-none focus:ring focus:ring-highlight-high sm:text-md"
-	class:active={showActive && $page.url.pathname === href}
+	class:active={$page.url.pathname.includes(href)}
 >
 	{label}
 </a>
 
-<style>
+<style lang="postcss">
 	.active {
 		@apply text-text;
 	}
