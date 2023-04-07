d8448f7be30512f5bfb9b9b8407e4c4cfb201efe
mvllow <mvllow@icloud.com>
2023-04-07 00:13:16 -0500

    add base design system
    This commit adds the Rosé Pine palette to tailwind via css variables.
    Other properties such as spacing, fonts and borders are also configured.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				"page-top": "var(--page-top)",
+				"page-gutter": "var(--page-gutter)",
+				"banner-height": "var(--banner-height)",
+				"header-height": "var(--header-height)",
+				"footer-height": "var(--footer-height)",
+			},
+			maxWidth: {
+				content: "var(--content-width)",
+			},
+			minHeight: {
+				content: "var(--content-height)",
+			},
+			fontFamily: {
+				display: "Pier Sans",
+			},
+			colors: {
+				base: "hsl(var(--color-base) / <alpha-value>)",
+				surface: "hsl(var(--color-surface) / <alpha-value>)",
+				overlay: "hsl(var(--color-overlay) / <alpha-value>)",
+				muted: "hsl(var(--color-muted) / <alpha-value>)",
+				subtle: "hsl(var(--color-subtle) / <alpha-value>)",
+				text: "hsl(var(--color-text) / <alpha-value>)",
+				love: "hsl(var(--color-love) / <alpha-value>)",
+				gold: "hsl(var(--color-gold) / <alpha-value>)",
+				rose: "hsl(var(--color-rose) / <alpha-value>)",
+				pine: "hsl(var(--color-pine) / <alpha-value>)",
+				foam: "hsl(var(--color-foam) / <alpha-value>)",
+				iris: "hsl(var(--color-iris) / <alpha-value>)",
+			},
+			borderColor: {
+				DEFAULT: "hsl(var(--color-muted) / 0.2)",
+			},
+			ringColor: {
+				DEFAULT: "hsl(var(--color-foam) / 0.2)",
+			},
+		},
 	},
-	plugins: [],
 };
