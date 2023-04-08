22fe6d575672024e238699cd7f0c5fcd775c11d0
mvllow <mvllow@icloud.com>
2023-04-08 13:15:36 -0500

    move astro components to svelte
    This commit moves all previous Astro components (not pages) to Svelte,
    improving the authoring experience when using Astro islands.
    
    A global Svelte store is used to track `Astro.url.pathname` because
    direct access to `Astro.*` is not available inside Svelte components.
    
    Alpine.js was also considered and enjoyed but the interoperability
    between Astro's frontmatter and Alpine's string-interpreted data did not
    prove to be too friendly.

---
+++ src/components/banner.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let links: [name: string, href: string][];
+</script>
+
+<aside
+	class="flex h-banner-height items-center overflow-x-auto whitespace-nowrap bg-love/10 px-3 text-center text-love"
+>
+	<div class="flex-1" />
+
+	{#each links as [name, href], index}
+		<a
+			{href}
+			class="text-sm font-medium transition hover:underline focus:outline-none focus:ring"
+		>
+			{name}
+		</a>
+
+		{#if index < links.length - 1}
+			<div class="mx-3 h-6 border-r border-love/20" />
+		{/if}
+	{/each}
+
+	<div class="flex-1" />
+</aside>
--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,14 +1,16 @@
----
-function isCurrentPage(url: string) {
-	return Astro.url.pathname === url;
-}
+<script lang="ts">
+	import { pathname } from "$store";
 
-const menu = [
-	["Themes", "/themes"],
-	["Palette", "/palette"],
-	["Resources", "/resources"],
-];
----
+	function isCurrentPage(url: string) {
+		return $pathname === url;
+	}
+
+	const menu = [
+		["Themes", "/themes"],
+		["Palette", "/palette"],
+		["Resources", "/resources"],
+	];
+</script>
 
 <header class="flex h-header-height justify-center px-page-gutter">
 	<div
@@ -23,15 +25,15 @@ const menu = [
 				<path
 					style="fill: hsl(var(--color-pine) / 0.5)"
 					d="M98.9824 152.25C94.1645 140.61 84.1877 131.253 71.1002 127.727L14.9408 112.599C7.82924 139.282 23.6151 166.976 50.2789 174.159L90.5058 184.995C93.3596 185.764 96.2625 184.961 98.2972 183.114C100.304 185.655 103.699 186.897 107.028 186.001L147.255 175.164C173.918 167.982 189.704 140.287 182.593 113.604L126.433 128.733C113.725 132.156 103.949 141.078 98.9824 152.25Z"
-				></path>
+				/>
 				<path
 					style="fill: hsl(var(--color-pine))"
 					d="M99 177.99C94.3676 155.116 74.1899 137.733 50 137.733L0 137.733C0 165.347 22.3858 188 50 188H98H100H148C175.614 188 198 165.347 198 137.733L148 137.733C123.81 137.733 103.632 155.116 99 177.99Z"
-				></path>
+				/>
 				<path
 					style="fill: hsl(var(--color-rose))"
 					d="M116.172 22.0991C111.969 13.1397 105.096 5.32759 95.9178 0L83.9001 20.9265C83.5434 21.5477 83.2015 22.1733 82.8743 22.8031C70.8317 12.8076 54.2791 8.62488 38.0001 13.0102L51.0053 61.8058C55.7428 79.5809 69.4827 92.5279 86.0109 97.068L85.9999 97.1091C90.3717 98.2868 94.7632 98.8465 99.0818 98.8417C103.405 98.8478 107.802 98.2881 112.178 97.1091L112.167 97.0679C128.696 92.5279 142.435 79.5809 147.173 61.8058L160.178 13.0102C144.287 8.72929 128.135 12.6132 116.172 22.0991Z"
-				></path>
+				/>
 			</svg>
 
 			<span
@@ -42,19 +44,17 @@ const menu = [
 
 		<nav aria-label="Main">
 			<ul role="list" class="flex items-center">
-				{
-					menu.map(([name, href]) => (
-						<li>
-							<a
-								href={href}
-								aria-current={isCurrentPage(href) ? "page" : undefined}
-								class="rounded-md px-2.5 py-1.5 text-[15px] font-semibold transition hover:bg-muted/10 focus:outline-none focus:ring focus:ring-inset"
-							>
-								{name}
-							</a>
-						</li>
-					))
-				}
+				{#each menu as [name, href], index}
+					<li>
+						<a
+							{href}
+							aria-current={isCurrentPage(href) ? "page" : undefined}
+							class="rounded-md px-2.5 py-1.5 text-[15px] font-semibold transition hover:bg-muted/10 focus:outline-none focus:ring focus:ring-inset"
+						>
+							{name}
+						</a>
+					</li>
+				{/each}
 			</ul>
 		</nav>
 	</div>
