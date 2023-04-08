3a458377ef9a000b72241a702a0becbd1f99a71c
mvllow <mvllow@icloud.com>
2023-04-08 14:14:06 -0500

    hide redundant role warning
    This commit ignores the "a11y-no-redundant-roles" svelte-check warning.
    Tailwind CSS removes the default list-style, making the role attribute
    necessary for VoiceOver to announce lists as such.
    
    https://tailwindcss.com/docs/preflight#accessibility-considerations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
 export default defineConfig({
-	integrations: [svelte()]
+	integrations: [
+		svelte({
+			onwarn: (warning, handler) => {
+				if (warning.code === "a11y-no-redundant-roles") return;
+				handler(warning);
+			},
+		}),
+	],
 });
