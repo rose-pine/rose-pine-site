73fed04fce94a6eb6b1290979617af7a5e8f3c00
mvllow <mvllow@icloud.com>
2023-04-08 16:57:40 -0500

    add editor-frame component

---
+++ src/components/editor-frame.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+	import { IconGitBranch } from "@tabler/icons-svelte";
+</script>
+
+<div
+	aria-hidden="true"
+	class="relative mx-auto grid h-80 w-full max-w-lg grid-rows-[auto,1fr,auto] overflow-hidden rounded-3xl border bg-base lg:mx-0 lg:h-96 lg:max-w-full"
+>
+	<div class="flex h-12 items-center space-x-2 px-4">
+		<div class="h-3.5 w-3.5 rounded-full bg-muted/20" />
+		<div class="h-3.5 w-3.5 rounded-full bg-muted/20" />
+		<div class="h-3.5 w-3.5 rounded-full bg-muted/20" />
+	</div>
+
+	<div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-pine" />
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-text" />
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-pine" />
+			<div class="h-3.5 w-14 shrink-0 rounded-full bg-gold" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-pine" />
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-text" />
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-pine" />
+			<div class="h-3.5 w-16 shrink-0 rounded-full bg-gold" />
+		</div>
+		<div
+			class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10"
+		/>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full bg-pine" />
+			<div class="h-3.5 w-20 shrink-0 rounded-full bg-rose" />
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-iris" />
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-iris" />
+			<div class="h-3.5 w-6 shrink-0 rounded-full bg-muted" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-6 shrink-0 rounded-full bg-subtle" />
+			<div class="h-3.5 w-12 shrink-0 rounded-full bg-subtle" />
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-subtle" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-love" />
+			<div class="h-3.5 w-16 shrink-0 rounded-full bg-text" />
+			<div class="h-3.5 w-12 shrink-0 rounded-full bg-gold" />
+			<div class="h-3.5 w-24 shrink-0 rounded-full bg-text" />
+		</div>
+		<div
+			class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10"
+		/>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-pine" />
+			<div class="h-3.5 w-14 shrink-0 rounded-full bg-text" />
+			<div class="h-3.5 w-12 shrink-0 rounded-full bg-iris" />
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-foam" />
+			<div class="h-3.5 w-6 shrink-0 rounded-full bg-muted" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-foam" />
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-gold" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-foam" />
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-rose" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-foam" />
+			<div class="h-3.5 w-14 shrink-0 rounded-full bg-gold" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-14 shrink-0 rounded-full bg-foam" />
+			<div class="h-3.5 w-16 shrink-0 rounded-full bg-gold" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-12 shrink-0 rounded-full"><!-- Indent --></div>
+			<div class="h-3.5 w-8 shrink-0 rounded-full bg-foam" />
+			<div class="h-3.5 w-10 shrink-0 rounded-full bg-gold" />
+		</div>
+		<div class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10">
+			<div class="h-3.5 w-6 shrink-0 rounded-full bg-muted" />
+		</div>
+		<div
+			class="flex h-7 items-center space-x-3 bg-base px-8 hover:bg-muted/10"
+		/>
+	</div>
+
+	<div
+		class="absolute bottom-0 flex h-8 w-full items-center space-x-1 rounded-b-3xl bg-surface px-4 supports-[backdrop-filter]:bg-surface/80 supports-[backdrop-filter]:backdrop-blur-md"
+	>
+		<IconGitBranch size={12} class="text-muted" />
+		<span class="font-mono text-xs text-subtle">main</span>
+	</div>
+</div>
