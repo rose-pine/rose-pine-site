dacb6c341fae17ed1732e9f3219d20fef1b68439
mvllow <mvllow@icloud.com>
2023-04-11 19:22:10 -0500

    comment on reasoning for disabling `a11y-no-redundant-roles` warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ export default defineConfig({
 	integrations: [
 		svelte({
 			onwarn: (warning, handler) => {
+				// Improve VoiceOver announcements with `role="list"` on lists
+				// with `list-style: none`
 				if (warning.code === "a11y-no-redundant-roles") return;
 				handler(warning);
 			},
