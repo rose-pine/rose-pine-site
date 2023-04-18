bad9ab188215f03e70a0797b6ea9cf3de92f72db
mvllow <mvllow@icloud.com>
2023-04-18 13:30:03 -0500

    replace spicy tag with star

--- src/components/themes-list.svelte
+++ src/components/themes-list.svelte
@@ -22,11 +22,34 @@
 					/>
 
 					{#if theme.featured}
-						<div
-							class="pointer-events-none rounded-full bg-gradient-to-br from-rose via-iris to-foam px-2 py-1 text-surface"
+						<span class="sr-only">Featured theme</span>
+						<svg
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							class="text-gold"
 						>
-							<p class="text-xs font-bold uppercase tracking-wide">Spicy</p>
-						</div>
+							<defs>
+								<linearGradient id="spicy">
+									<stop offset="0%" stop-color="hsl(var(--color-rose))" />
+									<stop offset="50%" stop-color="hsl(var(--color-iris))" />
+									<stop offset="100%" stop-color="hsl(var(--color-foam))" />
+								</linearGradient>
+							</defs>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path
+								d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
+								stroke-width="0"
+								fill="url(#spicy)"
+							/>
+						</svg>
 					{/if}
 				</div>
 
