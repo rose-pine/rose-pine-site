0a889634a17a46de1924973faa834a96d07c5525
mvllow <mvllow@icloud.com>
2023-04-19 14:03:57 -0500

    update selected tag styles

--- src/components/themes-tags-list.svelte
+++ src/components/themes-tags-list.svelte
@@ -25,20 +25,13 @@
 				fill="none"
 				stroke-linecap="round"
 				stroke-linejoin="round"
-				class="-ml-1 mr-1.5 text-gold"
+				class="-ml-1 mr-1.5"
 			>
-				<defs>
-					<linearGradient id="spicy">
-						<stop offset="0%" stop-color="hsl(var(--color-rose))" />
-						<stop offset="50%" stop-color="hsl(var(--color-iris))" />
-						<stop offset="100%" stop-color="hsl(var(--color-foam))" />
-					</linearGradient>
-				</defs>
 				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 				<path
 					d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
 					stroke-width="0"
-					fill="url(#spicy)"
+					fill="currentColor"
 				/>
 			</svg>
 			Featured
@@ -63,6 +56,6 @@
 
 <style lang="postcss">
 	[aria-current="true"] > button {
-		@apply bg-iris/10 text-iris;
+		@apply bg-rose/10 text-rose;
 	}
 </style>
