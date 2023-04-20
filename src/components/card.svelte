ae0b08cc02227948be3a5bc2070e5e3a0b76f627
mvllow <mvllow@icloud.com>
2023-04-19 19:02:53 -0500

    add link component
    This commit adds a link component to help achieve consistently styling.
    Design patterns are not 100% yet but this is more of an initial
    foundation.

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+	import Link from "$components/link.svelte";
+
 	export let id: Optional<string> = undefined;
 	export let href: Optional<string> = undefined;
 	export let linkText: Optional<string> = undefined;
 	export let size: "sm" | "lg" = "sm";
-
-	const isExternal = href?.startsWith("http");
 </script>
 
 <svelte:element
@@ -22,11 +22,7 @@
 		<div class="flex-1" />
 
 		<div class="pt-1.5">
-			<span
-				class="inline font-medium text-iris underline-offset-2 after:inline-block after:whitespace-pre hover:underline"
-				class:internal-link={!isExternal}
-				class:external-link={isExternal}>{linkText}</span
-			>
+			<Link nested {href}>{linkText}</Link>
 		</div>
 	{/if}
 </svelte:element>
@@ -37,12 +33,6 @@
 	}
 	.size-lg {
 		@apply rounded-3xl p-6 lg:p-10;
-	}
-	.internal-link {
-		@apply after:content-['_→'];
-	}
-	.external-link {
-		@apply after:content-['_↗'];
 	}
 	.hoverable:hover {
 		@apply bg-muted/10;
---
+++ src/components/link.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let href: Optional<string> = undefined;
+	export let decorations = true;
+	export let nested = false;
+
+	const element = href && !nested ? "a" : "span";
+	const isExternal = href?.startsWith("http");
+</script>
+
+<svelte:element
+	this={element}
+	{href}
+	class="inline font-medium text-iris underline-offset-2 after:inline-block after:whitespace-pre hover:underline"
+	class:internal-link={decorations && !isExternal}
+	class:external-link={decorations && isExternal}><slot /></svelte:element
+>
+
+<style lang="postcss">
+	.internal-link {
+		@apply after:content-['_→'];
+	}
+	.external-link {
+		@apply after:content-['_↗'];
+	}
+</style>
