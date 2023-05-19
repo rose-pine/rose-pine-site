aec380ec1761c03683e3108ae11e9af3284ce5e2
mvllow <mvllow@icloud.com>
2023-05-19 20:16:42 +0200

    wip: copy palette values from swatches
    This commit enables a work-in-progress interface for copying palette
    values while in the swatches view. The current implementation shows
    three buttons when hovering the swatch card, overlaying the content of
    the card. Further improvements should be made for a more consistent,
    reliable user experience.

---
+++ src/components/icons/check.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let size = 24;
+	export let stroke = 2;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	stroke-width={stroke}
+	stroke="currentColor"
+	fill="none"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	{...$$restProps}
+>
+	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	<path d="M5 12l5 5l10 -10" />
+</svg>
--- src/components/palette-swatches.svelte
+++ src/components/palette-swatches.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
+	import { variantKeys } from "@rose-pine/palette";
 	import Card from "$components/card.svelte";
+	import { IconCheck, IconCopy } from "$components/icons";
 	import { specification } from "$data/palette";
+	import { clipboard } from "$store";
 </script>
 
-<section class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
+<section class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
 	{#each specification as role}
-		<Card id={role.id}>
-			<div class="flex items-start justify-between gap-3">
+		<Card id={role.id} class="group relative overflow-hidden">
+			<div class="flex justify-between gap-1.5">
 				<div>
 					<h2
-						class="text-lg font-semibold capitalize tracking-tight target:ring"
+						class="text-lg font-semibold capitalize leading-tight tracking-tight target:ring"
 					>
 						{role.name}
 					</h2>
 
-					<p class="font-serif italic text-subtle">{role.description}</p>
+					<p class="mt-1 leading-snug">
+						<em class="font-serif text-subtle">{role.description}</em>
+					</p>
 				</div>
 
-				<div class="flex items-center -space-x-1.5 sm:space-x-1.5">
-					{#each role.colors as color}
+				<div class="flex shrink-0 items-center -space-x-1.5 self-start">
+					{#each role.colors as color, i}
 						<div
-							class="h-8 w-8 shrink-0 rounded-full border ring-2 ring-surface"
+							class="h-7 w-7 shrink-0 rounded-full border-2 ring-2 ring-surface transition dark:first:z-[3] dark:last:z-[1] dark:[&:nth-child(2)]:z-[2]"
 							style:background={color}
 						/>
 					{/each}
@@ -29,19 +34,23 @@
 
 			<div class="h-6" />
 
-			<ul
-				role="list"
-				class="flex-1 list-disc pl-[18px] leading-relaxed marker:text-muted"
-			>
-				{#each role.scenarios as scenario}
-					<li class="text-[15px] font-medium">{scenario}</li>
-				{/each}
-			</ul>
+			{#if role.content}
+				<p>
+					{@html role.content.replaceAll(/\*\*(.*?)\*\*/g, "<b>$1</b>")}
+				</p>
+			{:else}
+				<ul
+					role="list"
+					class="flex-1 list-disc pl-[18px] leading-relaxed marker:text-muted"
+				>
+					{#each role.scenarios as scenario}
+						<li class="text-[15px] font-medium">{scenario}</li>
+					{/each}
+				</ul>
+			{/if}
 
 			{#if role.tags}
-				<div class="h-6" />
-
-				<ul role="list" class="flex items-center space-x-1.5">
+				<ul role="list" class="mt-6 flex items-center space-x-1.5">
 					{#each role.tags as tag}
 						<li
 							class="scheme-{role.id} inline-flex rounded-md px-3 py-1.5 font-mono text-sm font-semibold"
@@ -51,6 +60,44 @@
 					{/each}
 				</ul>
 			{/if}
+
+			<div
+				class="absolute inset-0 z-20 flex items-center justify-center bg-surface p-3 opacity-0 transition duration-200 group-focus-within:opacity-100 group-hover:opacity-100 supports-[backdrop-filter]:bg-surface/80 supports-[backdrop-filter]:backdrop-blur-md"
+			>
+				<div class="flex items-center gap-3.5">
+					{#each role.colors as color, i}
+						{@const variantName = variantKeys[i]}
+
+						<div class="space-y-1.5 text-center">
+							<p
+								class="text-[11px] font-medium uppercase tracking-wide text-subtle"
+							>
+								{variantName}
+							</p>
+
+							<button
+								on:click={() => clipboard.copy(color, variantName)}
+								class="group/item flex flex-col items-center justify-center space-y-1.5"
+							>
+								<div
+									class="h-10 w-10 rounded-full border-2"
+									style:background={color}
+								/>
+
+								{#if $clipboard.position === variantName}
+									<div class="text-foam">
+										<IconCheck size={16} stroke={1.5} />
+									</div>
+								{:else}
+									<div class="text-muted">
+										<IconCopy size={16} stroke={1.5} />
+									</div>
+								{/if}
+							</button>
+						</div>
+					{/each}
+				</div>
+			</div>
 		</Card>
 	{/each}
 </section>
