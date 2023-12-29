b56f0414ba17d3132732e66b6b3fc7e9fab2d099
fvrests <fvrests@icloud.com>
2023-12-28 18:23:20 -0600

    fix: language icon (#130)
    closes #129

--- src/components/language-select.svelte
+++ src/components/language-select.svelte
@@ -36,12 +36,9 @@
 				stroke-linecap="round"
 				stroke-linejoin="round"
 			>
-				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-				<path d="M4 5h7" />
-				<path d="M9 3v2c0 4.418 -2.239 8 -5 8" />
-				<path d="M5 9c0 2.144 2.952 3.908 6.7 4" />
-				<path d="M12 20l4 -9l4 9" />
-				<path d="M19.1 18h-6.2" />
+				<path d="M4 6h7M9 6c0 4.418-2.239 8-5 8" />
+				<path d="M5 10c0 2.144 2.952 3.908 6.7 4M12 21l4-9 4 9M19.1 19h-6.2" />
+				<path d="m6 2 1 1" />
 			</svg>
 		</slot>
 
