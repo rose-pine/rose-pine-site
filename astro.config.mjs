d531fbd4dc4ccd14b808a4398f7cea10d5e458d8
mvllow <mvllow@icloud.com>
2025-03-08 12:16:31 -0600

    use experimental svg icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	experimental: {
+		svg: true,
+	},
 	vite: {
 		plugins: [tailwindcss()],
 	},
