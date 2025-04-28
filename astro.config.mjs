3aae889cee92e5276396e47fb278a9de53cba8b9
mvllow <mvllow@icloud.com>
2025-04-28 12:35:06 -0500

    add svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	integrations: [svelte()],
 });
