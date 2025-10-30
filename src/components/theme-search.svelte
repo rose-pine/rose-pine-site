4fdb467ffb48caea20244eeb3eb36fdedfde9de9
mvllow <mvllow@icloud.com>
2025-10-30 11:23:22 -0500

    use text cursor for entire theme search input

--- src/components/theme-search.svelte
+++ src/components/theme-search.svelte
@@ -15,7 +15,7 @@
 
 <label
 	for="theme-search"
-	class="flex h-14 items-center gap-3 rounded-(--input-radius) border border-muted/20 bg-surface px-3 transition [--input-inner-radius:calc(var(--input-radius)/2)] [--input-radius:1rem] focus-within:border-muted/40"
+	class="flex h-14 cursor-text items-center gap-3 rounded-(--input-radius) border border-muted/20 bg-surface px-3 transition [--input-inner-radius:calc(var(--input-radius)/2)] [--input-radius:1rem] focus-within:border-muted/40"
 >
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
