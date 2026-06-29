ed2226f109b68041c4ece0cba313cfcd65a5d589
PetrTech <55279432+PetrTech@users.noreply.github.com>
2026-06-29 23:32:41 +0200

    Add Czech translation.

---
+++ src/content/guides/cs/create-a-theme.md
@@ -0,0 +1,21 @@
+---
+title: Vytvořte motiv
+description: Obsáhlý návod, jak přinést Rosé Pine na vaše oblíbené platformy
+author: petrtech
+publishedAt: 2026-06-29T23:00:00+02:00
+updatedAt: 2026-06-29T23:00:00+02:00
+---
+
+Členové Rosé Pine podporují kreativitu a rozmanité interpretace naší omezené palety barev při tvorbě komunitních motivů. Pokud dáváte přednost strukturovanějšímu přístupu s průběžným vedením, vítáme oficiální motivy v organizaci Rosé Pine na GitHubu, kde vám tým pomůže, kdykoli to bude možné.
+
+Začněte s naší [šablonou repozitáře](https://github.com/rose-pine/rose-pine-template), kterou můžete použít jako základ pro svůj nový motiv.
+
+```sh
+gh repo create my-app --public --clone --template rose-pine/rose-pine-template
+```
+
+Pro snadný přístup k naší paletě barev a zahrnutí všech tří variant použijte [bloom](https://github.com/rose-pine/rose-pine-bloom), náš nástroj pro sestavení. Šablonu z existujícího souboru motivu vytvoříte pomocí `bloom init <jméno-souboru>`.
+
+Jakmile motiv vytvoříte, můžete [jej odeslat](/submit-a-theme) jako oficiální nebo komunitní motiv. V případě oficiálního motivu vás pozveme do naší organizace na GitHubu, abyste do ní mohli repozitář přesunout. Motiv se pak automaticky ukáže na webové stránce. V případě komunitního motivu jej na web přidáme na základě odeslaného formuláře. Žádné další kroky nejsou potřebné.
+
+Děkujeme za sdílení s komunitou, už se nemůžeme dočkat co vytvoříte!
---
+++ src/locales/cs.ts
@@ -0,0 +1,83 @@
+import { defineLocale } from "../utilities/i18n";
+
+export default defineLocale({
+	config: {
+		name: "Čeština",
+	},
+	translations: {
+		"shared.all": "Vše",
+		"shared.language": "Jazyk",
+
+		"header.nav_label": "Primární",
+
+		"footer.nav_label": "Sekundární",
+		"footer.heading.collections": "Výběry",
+		"footer.collection.photography": "Fotografie",
+		"footer.heading.resources": "Zdroje",
+		"footer.resource.create": "Vytvořte motiv",
+		"footer.resource.submit": "Přispět motivem",
+		"footer.resource.translate": "Přidat překlad",
+		"footer.heading.social": "Sociální sítě",
+
+		"nav.home": "Domů",
+		"nav.themes": "Motivy",
+		"nav.palette": "Barvy",
+		"nav.create": "Vytvořit",
+
+		"command.trigger": "Otevřít příkazovou nabídku",
+		"command.title": "Příkazová nabídka",
+		"command.description": "Vyhledávání na celém webu",
+		"command.search_placeholder": "Vyhledat stránky, motivy a barvy...",
+		"command.close": "Zavřít příkazovou nabídku",
+		"command.empty_results": "Žádné výsledky",
+		"command.feeling_lucky": "Zkusím štěstí",
+		"command.featured_themes": "Vybrané motivy",
+		"command.pages": "Stránky",
+		"command.community": "Komunita",
+
+		"home.title": "Něco krásného",
+		"home.description":
+			"Přírodní borovice, umělá kožešina a špetka Soho atmosféry pro elegantního minimalistu.",
+		"home.primary_action": "Procházet motivy",
+		"home.feature.colours_and_community.title": "Barvy a komunita",
+		"home.feature.colours_and_community.description":
+			"Rosé Pine je elegantní paleta barev používaná ve více než {{REPOS_COUNT}} vašich oblíbených aplikací díky úžasné práci našich přispěvatelů.",
+		"home.feature.passionately_subtle.title": "S citem pro jemnost",
+		"home.feature.passionately_subtle.description":
+			"S důrazem na kvalitu a účel, naše vybrané barvy mají za cíl inspirovat, nikoli přemoci. Těšíme se, až se o své výtvory podělíte se světem.",
+		"home.feature.inclusive_equal_diverse.title": "Inkluzivita, rovnost a rozmanitost",
+		"home.feature.inclusive_equal_diverse.description":
+			"Neustále se snažíme zajistit bezpečné a povzbuzující prostředí pro všechny. Jsme rádi, že jste tu.",
+
+		"themes.title": "Motivy",
+		"themes.description": "Pečlivě sestavený výběr možností přizpůsobení.",
+		"themes.ports": "Portů",
+		"themes.contributors": "Přispěvatelů",
+		"themes.search_placeholder": "Hledat motivy...",
+		"themes.search_focus_cue": "Stiskněte / pro hledání",
+
+		"photography.title": "Fotografie",
+		"photography.description":
+			"Pokud se zde na chvíli nepozastavíte, možná minete nádherný výhled.",
+
+		"palette.title": "Barvy",
+		"palette.description": "Vybrané barvy, navržené tak, aby vás inspirovaly.",
+		"palette.variants": "Varianty",
+		"palette.colours": "Barev",
+
+		"create.title": "Vytvořit",
+		"create.description":
+			"Návody, nástroje a inspirace pro tvorbu nádherných motivů.",
+		"create.also_available_in": "Také dostupné v:",
+
+		"article.authored_by": "Od",
+		"article.authored_on": ", {{date}}",
+		"article.updated_on": "(aktualizováno {{date}})",
+
+		"submit.title": "Přispět motivem",
+		"submit.description": "Přispějte svým motivem, aby mohl být přidán do Rosé Pine.",
+		"submit.form.link_label": "Odkaz na repozitář",
+		"submit.form.category_label": "Kategorie",
+		"submit.form.primary_action": "Odeslat",
+	},
+});
