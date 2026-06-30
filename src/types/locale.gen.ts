5de4badd371aad3b5b0cb79315080256b34b0486
mvllow <mvllow@icloud.com>
2026-06-30 09:21:34 -0500

    generate types

--- src/types/locale.gen.ts
+++ src/types/locale.gen.ts
@@ -1,2 +1,2 @@
 // Auto-generated — do not edit
-export type Locale = "ar" | "ca" | "de" | "en" | "es" | "fr" | "id" | "it" | "ko" | "nl" | "pt-BR" | "sv" | "tr" | "uk" | "zh-CN";
+export type Locale = "ar" | "ca" | "cs" | "de" | "en" | "es" | "fr" | "id" | "it" | "ko" | "nl" | "pt-BR" | "sv" | "tr" | "uk" | "zh-CN";
