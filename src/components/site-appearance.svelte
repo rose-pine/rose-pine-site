6b46a2345a06ae01e34b4fdb689cd344085aa0a7
mvllow <mvllow@icloud.com>
2026-07-29 09:51:36 -0500

    update theme switcher

--- src/components/site-appearance.svelte
+++ src/components/site-appearance.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
-	let themes = ["system", "main", "moon", "dawn"] as const;
+	type ThemeValue = "system" | "main" | "moon" | "dawn";
+	let themes: ThemeValue[] = ["system", "main", "moon", "dawn"];
 
 	let appearance = $state("");
 
 	onMount(() => {
 		appearance = localStorage.theme ?? "system";
 	});
+
+	let themeNameMap = {
+		system: "Match system",
+		main: "Rosé Pine",
+		moon: "Rosé Pine Moon",
+		dawn: "Rosé Pine Dawn",
+	} satisfies Record<ThemeValue, string>;
 
 	function updateTheme(theme: string) {
 		appearance = theme;
@@ -26,6 +34,8 @@
 	class="flex items-center gap-1.5 rounded-md border tonal-subtle px-1.5 py-1 text-sm font-medium capitalize transition hover:tonal-pressed-subtle"
 >
 	{#each themes as theme}
-		<option value={theme} disabled={appearance === theme}>{theme}</option>
+		<option value={theme} disabled={appearance === theme}
+			>{themeNameMap[theme]}</option
+		>
 	{/each}
 </select>
