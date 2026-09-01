658245151eafe0ae334596dbe7e7154b3b4a2df0
deepthought <deepthough@postech.ac.kr>
2026-09-01 14:37:52 +0900

    chore: add korean translation

--- src/locales/ko.ts
+++ src/locales/ko.ts
@@ -8,21 +8,77 @@ export default defineLocale({
 		"nav.home": "홈",
 		"nav.themes": "테마",
 		"nav.palette": "팔레트",
+		"nav.create": "만들기",
+		"nav.collections": "컬렉션",
+		"nav.collection.photography": "사진",
+		"nav.collection.made-with": "Rosé Pine으로 만든 작품",
+		"nav.language": "언어",
+		"nav.resources": "리소스",
+		"nav.resource.create": "테마 만들기",
+		"nav.resource.submit": "테마 제출하기",
+		"nav.resource.translate": "번역 추가하기",
+		"nav.social": "소셜",
+		"nav.header_label": "기본 네비게이션",
+		"nav.footer_label": "보조 네비게이션",
+
+		"command.trigger": "명령어 메뉴 열기",
+		"command.title": "명령어 메뉴",
+		"command.description": "사이트 전체 검색",
+		"command.search_placeholder": "페이지, 테마 또는 팔레트 검색...",
+		"command.close": "명령어 메뉴 닫기",
+		"command.empty_results": "검색 결과가 없습니다",
+		"command.feeling_lucky": "행운의 검색",
+		"command.recent_themes": "최근 업데이트된 테마",
+		"command.pages": "페이지",
+		"command.community": "커뮤니티",
 
 		"home.title": "특별한 아름다움",
 		"home.description":
-			"세련된 미니멀리스트를 위한 자연스러운 파인, 부드러운 질감, 그리고 소호의 감성",
+			"세련된 미니멀리스트를 위한 자연스러운 파인, 부드러운 질감, 그리고 소호의 감성.",
+		"home.primary_action": "테마 둘러보기",
+		"home.feature.colours_and_community.title": "색상 & 커뮤니티",
+		"home.feature.colours_and_community.description":
+			"Rosé Pine은 소중하고 관대한 기여자분들 덕분에 {{REPOS_COUNT}}개 이상의 자주 사용하는 앱에서 만날 수 있는 우아한 팔레트입니다.",
+		"home.feature.passionately_subtle.title": "열정적이면서도 은은하게",
+		"home.feature.passionately_subtle.description":
+			"품질과 목적에 집중하여 정선된 색상들은 부담을 주지 않으면서도 영감을 불어넣도록 설계되었습니다. 여러분의 창작물을 세상에 선보이기를 기대합니다.",
+		"home.feature.inclusive_equal_diverse.title": "포용, 평등, 다양성",
+		"home.feature.inclusive_equal_diverse.description":
+			"우리는 모두를 위해 안전하고 격려가 넘치는 환경을 만들기 위해 끊임없이 노력합니다. 여러분 모두를 환영하고 존중합니다.",
 
 		"themes.title": "테마",
-		"themes.description": "정성스럽게 만든 나만의 컬렉션",
-		"themes.search_placeholder": "테마 검색하기",
+		"themes.description": "정성스럽게 만든 개성 넘치는 컬렉션.",
+		"themes.all": "전체",
+		"themes.ports": "포트",
+		"themes.contributors": "기여자",
+		"themes.search_placeholder": "테마 검색하기...",
+		"themes.search_focus_cue": "/ 키를 눌러 검색",
+
+		"colophon.title": "판권 / 정보",
+		"colophon.description": "우리 정원의 뿌리.",
+
+		"photography.title": "사진",
+		"photography.description":
+			"잠시 멈춰 서지 않으면 멋진 풍경을 놓칠지도 모릅니다.",
+
+		"made-with.title": "Rosé Pine으로 만든 작품",
+		"made-with.description": "커뮤니티의 주요 작품들.",
 
 		"palette.title": "팔레트",
+		"palette.description": "영감을 주도록 정선된 색상들.",
+		"palette.variants": "변형 (Variants)",
+		"palette.colours": "색상",
 
-		"command.search_placeholder": "페이지, 테마 또는 팔레트 검색",
-		"command.empty_results": "검색 결과가 없습니다",
-		"command.pages": "페이지",
-		"command.recent_themes": "최근 업데이트된 테마",
-		"command.community": "커뮤니티",
+		"create.title": "만들기",
+		"create.description":
+			"아름다운 테마를 제작하기 위한 가이드, 도구 및 영감.",
+		"create.also_available_in": "다른 언어로 보기:",
+
+		"submit.title": "테마 제출하기",
+		"submit.description": "Rosé Pine에 추가할 테마를 보내주세요.",
+		"submit.form.link_label": "저장소 링크",
+		"submit.form.category_label": "카테고리",
+		"submit.form.primary_action": "제출하기",
 	},
 });
+
